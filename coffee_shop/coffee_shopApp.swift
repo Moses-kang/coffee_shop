ae78130ff8384f66972cdf519fa4059e811382af
Moses Kang <mkang41@asu.edu>
2021-12-20 00:37:42 -0700

    Initial Commit

---
+++ coffee_shop/coffee_shopApp.swift
@@ -0,0 +1,17 @@
+//
+//  coffee_shopApp.swift
+//  coffee_shop
+//
+//  Created by Moses Kang on 12/20/21.
+//
+
+import SwiftUI
+
+@main
+struct coffee_shopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
